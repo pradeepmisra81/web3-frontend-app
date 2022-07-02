3cafe3c515ea09a7eef74ee2613ba7d4cc7fb7a3
pradeep <pradeep.misra@gmail.com>
2022-07-02 18:50:42 +0530

    react training ep2

---
+++ src/Components/MyFirstComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../Styles/MyFirstComponent.css';
+
+const MyFirstComponent = () => {
+  return (
+    <div className="mycomponent">
+      <a href='/JavaScript'>JavaScript</a>
+      <a href='/css'>CSS</a>
+      <a href='/html'>HTML</a>
+      <a href='/jquery'>jQuery</a>
+      <a href='/react'>React</a>
+      <a href='/web3'>Web3</a>
+      <a href='/blockchain'>Blockchain</a>
+      <a href='/solidity'>Solidity</a>
+    </div>
+  );
+}
+
+export default MyFirstComponent;
